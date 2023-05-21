64179b446920294e4a30e52e7c380970656ccde1
Rakibul Islam <rakib011110@gmail.com>
2023-05-21 20:23:17 +0600

    home banner add

--- src/Components/Home/HomeBanner.jsx
+++ src/Components/Home/HomeBanner.jsx
@@ -1,94 +1,94 @@
-// import React, { useEffect } from "react";
-// import Courosel from "../Common/Courosel";
-// import { Player, Controls } from "@lottiefiles/react-lottie-player";
-// import Aos from "aos";
-// import "aos/dist/aos.css";
+import React, { useEffect } from "react";
+import Courosel from "../Common/Courosel";
+import { Player, Controls } from "@lottiefiles/react-lottie-player";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
-// const HomeBanner = () => {
-//   useEffect(() => {
-//     Aos.init();
-//   }, []);
+const HomeBanner = () => {
+  useEffect(() => {
+    Aos.init();
+  }, []);
 
-//   return (
-//     <div data-aos-duration="1000" data-aos="slide-up">
-//       <div className="mt-5" data-aos="fade">
-//         <div className="flex  mx-auto flex-col md:flex-row bg-gray-100">
-//           <div className="md:w-1/2">
-//             <a href="#" className="group relative block bg-black">
-//               <img
-//                 alt="Developer"
-//                 src="https://img.freepik.com/premium-photo/closeup-photo-beautiful-little-lady-holding-arms-cheekbones-exciting-mood-newyear-discounts-wear-jeans-overall-white-t-shirt-isolated-red-vivid-color-background_274222-17784.jpg"
-//                 className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
-//               />
+  return (
+    <div data-aos-duration="1000" data-aos="slide-up">
+      <div className="mt-5" data-aos="fade">
+        <div className="flex  mx-auto flex-col md:flex-row bg-gray-100">
+          <div className="md:w-1/2">
+            <a href="#" className="group relative block bg-black">
+              <img
+                alt="Developer"
+                src="https://img.freepik.com/premium-photo/closeup-photo-beautiful-little-lady-holding-arms-cheekbones-exciting-mood-newyear-discounts-wear-jeans-overall-white-t-shirt-isolated-red-vivid-color-background_274222-17784.jpg"
+                className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
+              />
 
-//               <div className="relative p-4 sm:p-6 lg:p-8">
-//                 <p className="text-sm font-bold uppercase tracking-widest text-red-700">
-//                   WELCOME TO
-//                 </p>
+              <div className="relative p-4 sm:p-6 lg:p-8">
+                <p className="text-sm font-bold uppercase tracking-widest text-red-700">
+                  WELCOME TO
+                </p>
 
-//                 <p className="text-xl font-bold text-white sm:text-2xl">
-//                   <span className="">Toy </span> STORE{" "}
-//                 </p>
+                <p className="text-xl font-bold text-white sm:text-2xl">
+                  <span className="">Toy </span> STORE{" "}
+                </p>
 
-//                 <div className="mt-32 sm:mt-48 lg:mt-64">
-//                   <div className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100">
-//                     <p className="text-sm text-white">
-//                       Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-//                       Omnis perferendis hic asperiores quibusdam quidem
-//                       voluptates doloremque reiciendis nostrum harum.
-//                       Repudiandae?
-//                     </p>
-//                   </div>
-//                 </div>
-//                 <Player
-//                   className=" rounded-lg mb-4 sm:w-[300px] sm:h-[200px]"
-//                   autoplay
-//                   loop
-//                   src="https://assets1.lottiefiles.com/packages/lf20_42kpqpHRFU.json"
-//                   // style={{ height: "500px", width: "700px" }}
-//                 ></Player>
-//                 <div>
-//                   <h1 className="text-2xl font-bold text-red-600">
-//                     {" "}
-//                     CHOOSE YOUR FAVOURITEBABY TOYS
-//                   </h1>
-//                 </div>
-//               </div>
-//             </a>
-//           </div>
+                <div className="mt-32 sm:mt-48 lg:mt-64">
+                  <div className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100">
+                    <p className="text-sm text-white">
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                      Omnis perferendis hic asperiores quibusdam quidem
+                      voluptates doloremque reiciendis nostrum harum.
+                      Repudiandae?
+                    </p>
+                  </div>
+                </div>
+                <Player
+                  className=" rounded-lg mb-4 sm:w-[300px] sm:h-[200px]"
+                  autoplay
+                  loop
+                  src="https://assets1.lottiefiles.com/packages/lf20_42kpqpHRFU.json"
+                  // style={{ height: "500px", width: "700px" }}
+                ></Player>
+                <div>
+                  <h1 className="text-2xl font-bold text-red-600">
+                    {" "}
+                    CHOOSE YOUR FAVOURITEBABY TOYS
+                  </h1>
+                </div>
+              </div>
+            </a>
+          </div>
 
-//           <div className="md:w-1/2 flex items-center justify-center p-8">
-//             <div className="text-center">
-//               <div>
-//                 <Player
-//                   className=" rounded-lg  sm:w-[400px] sm:h-[300px]"
-//                   autoplay
-//                   loop
-//                   src="https://assets6.lottiefiles.com/packages/lf20_qcdojejs.json"
-//                   // style={{ height: "500px", width: "700px" }}
-//                 ></Player>
-//               </div>
-//               <h2 className="text-4xl font-bold mb-4  text-blue-600">
-//                 WELCOME TO OUR TOYS WEBSITE
-//               </h2>
-//               <p className="text-xl mb-4">
-//                 BABY TOYS is any nutrient-rich material consumed or absorbed by
-//                 humans, animals, or plants in order to sustain life and growth.
-//                 The major sources ofBABY TOYS are animals and plants. It is
-//                 consumed because it
-//               </p>
-//               <a
-//                 href="#"
-//                 className="bg-blue-600 text-white py-2 px-4 rounded hover:bg-red-600"
-//               >
-//                 Learn More
-//               </a>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+          <div className="md:w-1/2 flex items-center justify-center p-8">
+            <div className="text-center">
+              <div>
+                <Player
+                  className=" rounded-lg  sm:w-[400px] sm:h-[300px]"
+                  autoplay
+                  loop
+                  src="https://assets6.lottiefiles.com/packages/lf20_qcdojejs.json"
+                  // style={{ height: "500px", width: "700px" }}
+                ></Player>
+              </div>
+              <h2 className="text-4xl font-bold mb-4  text-blue-600">
+                WELCOME TO OUR TOYS WEBSITE
+              </h2>
+              <p className="text-xl mb-4">
+                BABY TOYS is any nutrient-rich material consumed or absorbed by
+                humans, animals, or plants in order to sustain life and growth.
+                The major sources ofBABY TOYS are animals and plants. It is
+                consumed because it
+              </p>
+              <a
+                href="#"
+                className="bg-blue-600 text-white py-2 px-4 rounded hover:bg-red-600"
+              >
+                Learn More
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-// export default HomeBanner;
+export default HomeBanner;
